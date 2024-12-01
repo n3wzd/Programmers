2ad1d634c3ba65679aa2ddace43a1e92fad70f95
n3wzd <87504447+n3wzd@users.noreply.github.com>
2024-12-01 17:07:28 +0900

    Create 연습문제 - 두 원 사이의 정수 쌍.cpp

---
+++ 연습문제 - 두 원 사이의 정수 쌍.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <cmath>
+using namespace std;
+typedef long long ll;
+
+ll C(ll r, bool outline) {
+    ll a = 0, da = 0;
+    for(ll x = 1, y = r; x <= r; x++) {
+        while((outline ? r * r < x * x + y * y : r * r <= x * x + y * y) && y > 0)
+            da++, y--;
+        a += da;
+    }
+    return (2 * r + 1) * (2 * r + 1) - (a * 4 + (outline ? 0 : 4));
+}
+
+ll solution(int r1, int r2) {
+    return C(r2 , 1) - C(r1, 0);
+}
