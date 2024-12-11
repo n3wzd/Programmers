9c4ef66de2dd2e30d338a7eddff8dda71df13ea8
n3wzd <87504447+n3wzd@users.noreply.github.com>
2024-12-11 14:07:33 +0900

    Update 연습문제 - 최고의 집합.cpp

--- 연습문제 - 최고의 집합.cpp
+++ 연습문제 - 최고의 집합.cpp
@@ -1,24 +1,14 @@
-#include <string>
 #include <vector>
 using namespace std;
-const int INF = 1e6;
-int DP[20001];
-string S;
 
-bool cmp(string tk, int hi) {
-    for(int i = 0; i < tk.size(); i++)
-        if(S[hi - tk.size() + i] != tk[i])
-            return 0;
-    return 1;
-}
-
-int solution(vector<string> strs, string t) {
-    S = t; int N = S.size();
-    for(int n = 1; n <= N; n++) {
-        DP[n] = INF;
-        for(auto tk : strs)
-            if(n >= tk.size() && S[n - 1] == tk.back() && cmp(tk, n))
-                DP[n] = min(DP[n], DP[n - tk.size()] + 1);
-    }
-    return DP[N] == INF ? -1 : DP[N];
+vector<int> solution(int n, int s) {
+    if(s < n)
+        return { -1 };
+    vector<int> answer;
+    int a = s / n, d = s - (n * a);
+    for(int i = 0; i < n; i++)
+        answer.push_back(a);
+    for(int i = 0; i < d; i++)
+        answer[n - 1 - i] += 1;
+    return answer;
 }
