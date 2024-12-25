e47c36a83e96bb15bf11267655ae702c83fd6bcf
n3wzd <87504447+n3wzd@users.noreply.github.com>
2024-12-25 23:39:20 +0900

    Create 연습문제 - 과일 장수.cpp

---
+++ 연습문제 - 과일 장수.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(int k, int m, vector<int> score) {
+    int answer = 0;
+    sort(score.begin(), score.end(), greater<int>());
+    for(int i = 0; i < score.size(); i += m)
+        if(i + m - 1 < score.size())
+            answer += score[i + m - 1] * m;
+    return answer;
+}
